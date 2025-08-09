f556fe0f6477ebafbbdd5b0788b85686e2c38b86
Srujan1 <srujanyadav95@gmail.com>
2025-08-09 23:12:02 +0530

    storing user data in local storage using persist

--- src/store/useAuthStore.js
+++ src/store/useAuthStore.js
@@ -1,38 +1,44 @@
 import { signInWithEmailAndPassword, signOut } from "firebase/auth";
 import { create } from "zustand";
 import { auth } from "../firebase/firebase";
+import { persist, createJSONStorage } from "zustand/middleware";
 
-const useAuthStore = create((set) => ({
-  user: null,
-  loading: false,
-  error: null,
-  setLoading: (value) => set({ loading: value }),
-  login: async (email, password) => {
-    try {
-      const userCredential = await signInWithEmailAndPassword(
-        auth,
-        email,
-        password
-      );
+const useAuthStore = create(
+  persist(
+    (set) => ({
+      user: null,
+      loading: false,
+      error: null,
+      setLoading: (value) => set({ loading: value }),
+      login: async (email, password) => {
+        try {
+          const userCredential = await signInWithEmailAndPassword(
+            auth,
+            email,
+            password
+          );
 
-      set({ user: userCredential.user, error: null });
-      return userCredential.user;
-    } catch (e) {
-      set({ error: e.message, loading: false });
-      throw e;
-    }
-  },
-  logout: async () => {
-    try {
-      set({ user: null });
-      await signOut(auth);
-    } catch (e) {
-      set({ error: e.message });
-    }
-  },
+          set({ user: userCredential.user, error: null });
+          return userCredential.user;
+        } catch (e) {
+          set({ error: e.message, loading: false });
+          throw e;
+        }
+      },
+      logout: async () => {
+        try {
+          set({ user: null });
+          await signOut(auth);
+        } catch (e) {
+          set({ error: e.message });
+        }
+      },
 
-  setUser: (userData) => set({ user: userData, loading: false }),
-  clearUser: () => set({ user: null }),
-}));
+      setUser: (userData) => set({ user: userData, loading: false }),
+      clearUser: () => set({ user: null }),
+    }),
+    { name: "auth-store", storage: createJSONStorage(() => localStorage) }
+  )
+);
 
 export default useAuthStore;
